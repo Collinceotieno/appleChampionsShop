fae63c3a356c4219ac600dda3ba08e2370efd33d
CollinceOtieno <otienocollince301@gmail.com>
2023-11-11 17:34:50 +0300

    finished working on products

--- products/views.py
+++ products/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from pyexpat.errors import messages
+from django.contrib import messages
+
+from products.forms import ProductForm
+from .models import Product
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    all_products = Product.objects.all()
+    context = {"products": all_products}
+    return render(request, 'products/products.html', context)
 
 
 def add_products(request):
-    return render(request, 'products/add-products.html')
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product saved successfully')
+        else:
+            messages.error(request, 'Product saving failed')
+            return redirect("add-products-url")
+    else:
+        form = ProductForm()
+    return render(request, 'products/add-products.html', {'form': form})
 
 
 def update_products(request):
