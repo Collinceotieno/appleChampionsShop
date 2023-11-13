c6de366dcd71911a53a2d468a9069f3a54265d48
CollinceOtieno <otienocollince301@gmail.com>
2023-11-13 11:38:35 +0300

    added delete update and pay

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('add-products/', my_views.add_products, name='add-products-url'),
     path('update-products/', my_views.update_products, name='view-products-url'),
     path('shop/', my_views.shop, name='shop-url'),
+    path('delete/<id>', my_views.delete, name='delete-url'),
+    path('update/<id>', my_views.update_products, name='update-url'),
+    path('pay/<id>', my_views.pay, name='pay-url'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,9 +1,14 @@
+from django.contrib.sites import requests
 from django.shortcuts import render, redirect
 from pyexpat.errors import messages
 from django.contrib import messages
-
 from products.forms import ProductForm
 from .models import Product
+
+import requests
+from django.http import HttpResponse
+from requests.auth import HTTPBasicAuth
+from .credentials import *
 
 
 def products(request):
@@ -26,9 +31,58 @@ def add_products(request):
     return render(request, 'products/add-products.html', {'form': form})
 
 
-def update_products(request):
-    return render(request, 'products/update-products.html')
+def update_products(request, id):
+    product = Product.objects.get(id=id)
+    if request.method == "POST":
+        product_name = request.POST.get('name')
+        product_qtty = request.POST.get('qtty')
+        product_price = request.POST.get('price')
+        product_desc = request.POST.get('desc')
+        product_image = request.FILES.get('image')
+        product.name = product_name
+        product.qtty = product_qtty
+        product.price = product_price
+        product.desc = product_desc
+        product.image = product_image
+        product.save()
+        messages.success(request, 'Product updated successfully')
+        return redirect('products-url')
+    return render(request, 'products/update-products.html', {'product': product})
 
 
 def shop(request):
     return render(request, 'products/shop.html')
+
+
+def delete(request, id):
+    product = Product.objects.get(id=id)
+    product.delete()
+    messages.success(request, 'Product deleted successfully')
+    return redirect('products-url')
+
+
+def pay(request, id):
+    product = Product.objects.get(id=id)
+    if request.method == "POST":
+        phone = request.POST['phone']
+        amount = product.price
+        access_token = MpesaAccessToken.validated_mpesa_access_token
+        api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+        headers = {"Authorization": "Bearer %s" % access_token}
+        request = {
+            "BusinessShortCode": LipanaMpesaPassword.Business_short_code,
+            "Password": LipanaMpesaPassword.decode_password,
+            "Timestamp": LipanaMpesaPassword.lipa_time,
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": amount,
+            "PartyA": phone,
+            "PartyB": LipanaMpesaPassword.Business_short_code,
+            "PhoneNumber": phone,
+            "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
+            "AccountReference": "PYMENT001",
+            "TransactionDesc": "School fees"
+        }
+
+        response = requests.post(api_url, json=request, headers=headers)
+        return HttpResponse("success")
+    return render(request, 'products/pay.html', {'product': product})
